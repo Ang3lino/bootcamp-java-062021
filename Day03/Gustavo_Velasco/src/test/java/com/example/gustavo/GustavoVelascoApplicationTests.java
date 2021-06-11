03cd4afd82bd3a4f72f7ccd6e24175354d846dac
gus <guskrull@gmail.com>
2021-06-11 14:26:17 -0500

    Gustavo Challenge

---
+++ Day03/Gustavo_Velasco/src/test/java/com/example/gustavo/GustavoVelascoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gustavo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustavoVelascoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
